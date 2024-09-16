4493028e8f904ba3684ca7e35382b3635446b26f
kimdan94 <123554665+kimdan94@users.noreply.github.com>
2024-09-16 20:29:48 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1 ; i <= n ; i++){
+            if((6 * i)% n == 0){
+                answer = i;
+                break; //만족할 경우 함수 자체를 탈출하도록함
+            }
+        }
+        return answer;
+    }
+}
