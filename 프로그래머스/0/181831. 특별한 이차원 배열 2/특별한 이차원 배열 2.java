e034d2c547cf14e175d60936532c3e9a80c63b8e
kimdan94 <123554665+kimdan94@users.noreply.github.com>
2024-11-04 14:26:03 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.44 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,15 +1,17 @@
 class Solution {
     public int solution(int[][] arr) {
-        int n = arr.length;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j] != arr[j][i]){
-                    return 0;
+        int n = arr.length;  // 행의 개수
+
+        for (int i = 0; i < n; i++) {
+            // 각 행의 열 개수를 확인하여 그 범위 안에서만 비교
+            for (int j = 0; j < arr[i].length; j++) {
+                // 대칭 여부 확인
+                if (i < arr[j].length && arr[i][j] != arr[j][i]) {
+                    return 0;  // 대칭이 아니면 0을 반환
                 }
             }
         }
         
-        return 1;
+        return 1;  // 모든 요소가 대칭을 이루면 1을 반환
     }
 }
